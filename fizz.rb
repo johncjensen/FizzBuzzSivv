3dce83e1f2d9d04473e4572046807b39a31018b5
John Jensen MBP <johncjensen@hotmail.com>
2014-01-27 08:39:12 -0800

    setup methods

--- fizz.rb
+++ fizz.rb
@@ -1,23 +1,31 @@
-require 'colorize'
+class Fizz
+  require 'colorize'
 
-puts 'give me a number'
-
-num = gets.chomp.to_i
-add_two_hundred = num + 200
-
-until num > add_two_hundred
-  print num
-  if (num % 3 == 0) && (num % 5 == 0) && (num % 7 == 0)
-    print ' FizzBuzzSivv'.colorize(:light_blue)
-  elsif (num % 3 == 0) && (num % 5 == 0)
-    print ' FizzBuzz'.colorize(:green)
-  elsif num % 5 == 0
-    print ' Buzz'.colorize(:blue)
-  elsif num % 3 == 0
-    print ' Fizz'.colorize(:red)
-  elsif num % 7 == 0
-    print ' Sivv'.colorize(:yellow)
+  def initalize
   end
-  puts
-  num += 1
+
+  def fizzbuzzlogic
+    puts 'give me a number'
+    num = gets.chomp.to_i
+    add_two_hundred = num + 200
+    until num > add_two_hundred
+      print num
+      if (num % 3 == 0) && (num % 5 == 0) && (num % 7 == 0)
+        print ' FizzBuzzSivv'.colorize(:light_blue)
+      elsif (num % 3 == 0) && (num % 5 == 0)
+        print ' FizzBuzz'.colorize(:green)
+      elsif num % 5 == 0
+        print ' Buzz'.colorize(:blue)
+      elsif num % 3 == 0
+        print ' Fizz'.colorize(:red)
+      elsif num % 7 == 0
+        print ' Sivv'.colorize(:yellow)
+      end
+      puts
+      num += 1
+    end
+  end
 end
+
+fizzy = Fizz.new
+fizzy.fizzbuzzlogic
