7bf096d7f25424bda34ab14d8afcb0338a975984
John Jensen MBP <johncjensen@hotmail.com>
2014-01-27 08:31:15 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Extensible FizzBuzz(Sivv)
+The objective of this was to extend the function of a basic FizzBuzz application and take user input.
+
+**Logic it follows:**
+
+If divisible by 3, 5 & 7 print FizzBuzzSivv
+
+If divisible by 3 & 5    print FizzBuzz
+
+If divisible by 3        print Fizz
+
+If divisible by 5        print Buzz
+
+If divisible by 7        print Sivv
---
+++ fizz.rb
@@ -0,0 +1,23 @@
+require 'colorize'
+
+puts 'give me a number'
+
+num = gets.chomp.to_i
+add_two_hundred = num + 200
+
+until num > add_two_hundred
+  print num
+  if (num % 3 == 0) && (num % 5 == 0) && (num % 7 == 0)
+    print ' FizzBuzzSivv'.colorize(:light_blue)
+  elsif (num % 3 == 0) && (num % 5 == 0)
+    print ' FizzBuzz'.colorize(:green)
+  elsif num % 5 == 0
+    print ' Buzz'.colorize(:blue)
+  elsif num % 3 == 0
+    print ' Fizz'.colorize(:red)
+  elsif num % 7 == 0
+    print ' Sivv'.colorize(:yellow)
+  end
+  puts
+  num += 1
+end
